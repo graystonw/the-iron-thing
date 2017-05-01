44aa72979a8a1a07e275c1682976b7419045d91e
Grayston Ulery <graystonulery@grayston-ulerys-macbook.local>
2017-05-01 01:16:02 -0400

    Successfully connected via Alamofire

--- ThroneBuilder/ViewController.swift
+++ ThroneBuilder/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://thronesdb.com/api/public/card/01001.json").responseJSON
+            { response in
+                print(response.request)
+                print(response.response)
+                print(response.data)
+                print(response.result)
+                if let json = response.result.value{
+                    print("JSON: \(json)")
+                }
+            }
     }
 
     override func didReceiveMemoryWarning() {
